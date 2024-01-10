ed7981e962f67ed719c8900106790efe33333ca1
Adamkadaban <adamkadaban@gmail.com>
2024-01-10 00:29:21 -0500

    add cloudfox to setup script

--- personal-scripts/adam/setup.sh
+++ personal-scripts/adam/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-
 pip3 install pipx
-apt install xclip
+apt install xclip golang -y
 pipx install git+https://github.com/Pennyw0rth/NetExec
+go install github.com/BishopFox/cloudfox@latest
+
 
 echo bashrc_addons >> ~/.bashrc
