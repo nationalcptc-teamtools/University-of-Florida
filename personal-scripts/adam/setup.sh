dae6f55dcebb5d93434353e60b2eea254305e56b
Adamkadaban <adamkadaban@gmail.com>
2024-01-10 11:07:02 -0500

    add batcat and less alias

--- personal-scripts/adam/setup.sh
+++ personal-scripts/adam/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip3 install pipx
-apt install xclip golang -y
+apt install xclip golang bat -y
 pipx install git+https://github.com/Pennyw0rth/NetExec
 pipx install git+https://github.com/Adamkadaban/NTLMCrack
 go install github.com/BishopFox/cloudfox@latest
