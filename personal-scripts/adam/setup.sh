1fdc86d0823ebe0c0122eaa9c8ad14d128eae84a
Adamkadaban <adamkadaban@gmail.com>
2024-01-10 00:36:20 -0500

    add ntlmcrack to setup script

--- personal-scripts/adam/setup.sh
+++ personal-scripts/adam/setup.sh
@@ -3,6 +3,7 @@
 pip3 install pipx
 apt install xclip golang -y
 pipx install git+https://github.com/Pennyw0rth/NetExec
+pipx install git+https://github.com/Adamkadaban/NTLMCrack
 go install github.com/BishopFox/cloudfox@latest
 
 
