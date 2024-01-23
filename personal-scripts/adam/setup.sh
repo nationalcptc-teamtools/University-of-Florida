bb16514ea9e5ada287de5f2bc8b24a2a98689881
Adamkadaban <adamkadaban@gmail.com>
2024-01-22 20:11:31 -0500

    silent wget

--- personal-scripts/adam/setup.sh
+++ personal-scripts/adam/setup.sh
@@ -24,7 +24,7 @@ pipx ensurepath
 # cloudfox
 # go install github.com/BishopFox/cloudfox@latest
 cloudfox_latest=$(curl -s -I https://github.com/bishopfox/cloudfox/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}')
-wget "https://github.com/BishopFox/cloudfox/releases/download/$cloudfox_latest/cloudfox-linux-amd64.zip"
+wget -q "https://github.com/BishopFox/cloudfox/releases/download/$cloudfox_latest/cloudfox-linux-amd64.zip"
 unzip cloudfox-linux-amd64.zip
 mv cloudfox/cloudfox /usr/bin
 rm -r cloudfox cloudfox-linux-amd64.zip
