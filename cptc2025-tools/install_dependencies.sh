14e1c83d6c6ae05c32c8a6cb2d5f5f54c3028d09
Ayden Colby <colbyayden@gmail.com>
2024-10-09 19:44:09 -0400

    dependency script

---
+++ cptc2025-tools/install_dependencies.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "Installing dependencies..."
+
+# List of dependencies to install
+dependencies=("sshfs" "asciinema" "tmux" "sliver")
+
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+# Update package list and install dependencies
+echo "Updating package list..."
+apt-get update -y
+
+echo "Installing dependencies..."
+for package in "${dependencies[@]}"; do
+  if ! dpkg -l | grep -qw "$package"; then
+    echo "Installing $package..."
+    apt-get install -y "$package"
+  else
+    echo "$package is already installed"
+  fi
+done
+
+echo "All dependencies are installed."
