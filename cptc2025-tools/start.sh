3024c8ac8c50100c33101b6fec8344f1da0649ca
Ayden Colby <colbyayden@gmail.com>
2024-10-09 20:22:57 -0400

    Client wait for server

--- cptc2025-tools/start.sh
+++ cptc2025-tools/start.sh
@@ -4,6 +4,10 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 sudo bash "$DIR/install_dependencies.sh"
 
+# Wait for server setup to finish and press Enter
+echo "Wait here for server setup to finish... hit enter to continue"
+read -r  # Wait for user input
+
 sudo bash "$DIR/connect_sshfs_client.sh"
 
 bash "$DIR/start_recorded_tmux.sh"
