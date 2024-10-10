da19d3126db443b9f28a3007e3bc6bf39f462c5d
Ayden Colby <colbyayden@gmail.com>
2024-10-09 20:18:31 -0400

    bug2

--- cptc2025-tools/start_server.sh
+++ cptc2025-tools/start_server.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-cd "${dirname BASH_SOURCE[0]}"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-chmod +x install_dependencies.sh
-sudo ./install_dependencies.sh
+sudo bash "$DIR/install_dependencies.sh"
 
-chmod +x setup_sshfs_server.sh
-sudo ./setup_sshfs_server.sh
+sudo bash "$DIR/setup_sshfs_server.sh"
 
-chmod +x start_recorded_tmux.sh
-./start_recorded_tmux.sh
+bash "$DIR/start_recorded_tmux.sh"
