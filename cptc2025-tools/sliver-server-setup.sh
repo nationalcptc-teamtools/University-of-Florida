e7a85df6dc1145d75e8d89196e3bf4abc90d0009
Ayden Colby <colbyayden@gmail.com>
2024-10-11 01:00:05 -0400

    sliver autosetup

--- cptc2025-tools/sliver-server-setup.sh
+++ cptc2025-tools/sliver-server-setup.sh
@@ -1,1 +1,14 @@
-echo "Hello Server"
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Path to the config.yaml file
+CONFIG_FILE="$DIR/config.yaml"
+# Extract variables from the YAML file
+shared_dir=$(yq -r '.local_mount_dir' "$CONFIG_FILE")
+server_ip=$(yq -r '.server_ip' "$CONFIG_FILE")
+
+mkdir -p $shared_dir/sliver/profile
+mkdir -p $shared_dir/sliver/payloads
+
+chmod +x $DIR/sliver_expect_script.exp
+$DIR/sliver_expect_script.exp $server_ip $shared_dir
