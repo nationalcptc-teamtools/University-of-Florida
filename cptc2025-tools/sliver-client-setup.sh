b2161c2ff143355abf01928173d26721a3f379f0
Ayden Colby <colbyayden@gmail.com>
2024-10-11 01:00:05 -0400

    sliver autosetup

--- cptc2025-tools/sliver-client-setup.sh
+++ cptc2025-tools/sliver-client-setup.sh
@@ -1,1 +1,11 @@
-echo "Hello client"
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Path to the config.yaml file
+CONFIG_FILE="$DIR/config.yaml"
+# Extract variables from the YAML file
+shared_dir=$(yq -r '.local_mount_dir' "$CONFIG_FILE")
+username=$(yq -r '.username' "$CONFIG_FILE")
+
+sudo sliver-client import $shared_dir/$username.cfg
+sudo sliver-client
