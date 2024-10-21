a5ec7f50106988625629dcbfc61df0911e06d6e3
Ayden Colby <colbyayden@gmail.com>
2024-10-21 17:16:23 -0400

    Reattach and record tmux

---
+++ cptc2025-tools/reattach.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Check if yq is installed (a command-line YAML processor)
+if ! command -v yq &> /dev/null
+then
+    echo "yq could not be found. Please install yq to proceed."
+    exit 1
+fi
+
+# Path to the config.yaml file
+CONFIG_FILE="config.yaml"
+# Extract variables from the YAML file
+shared_dir=$(yq -r '.local_mount_dir' "$CONFIG_FILE")
+username=$(yq -r '.username' "$CONFIG_FILE")
+
+echo "Starting recording..."
+cd ~
+asciinema rec terminal.cast --command="tmux a" --append
