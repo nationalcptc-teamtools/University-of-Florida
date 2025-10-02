d68d0babd2283e5363de4f6689f88c832b6896c5
yhuang3-uf <115114652+yhuang3-uf@users.noreply.github.com>
2025-10-02 19:08:43 -0400

    Make script more automatic

--- personal-scripts/agenttesla/dockerbookworm.sh
+++ personal-scripts/agenttesla/dockerbookworm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install ca-certificates curl
+apt-get install -y ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -8,4 +8,4 @@ chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian bookworm stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
