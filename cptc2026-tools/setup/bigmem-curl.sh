3f5e7427898ed3c006a0cb90d1a9fbae03554ba4
yhuang3-uf <115114652+yhuang3-uf@users.noreply.github.com>
2025-10-09 15:25:06 -0400

    Add files to set up machines with large amounts of memory

---
+++ cptc2026-tools/setup/bigmem-curl.sh
@@ -0,0 +1,3 @@
+curl -LO https://github.com/Mozilla-Ocho/llamafile/releases/download/0.9.3/llamafile-0.9.3
+curl -LO https://huggingface.co/Mozilla/Phi-3-mini-4k-instruct-llamafile/resolve/main/Phi-3-mini-4k-instruct.Q4_K_S.llamafile?download=true
+curl -LO https://huggingface.co/Mozilla/Qwen2.5-7B-Instruct-1M-llamafile/resolve/main/Qwen2.5-7B-Instruct-1M-Q4_K_M.llamafile?download=true
---
+++ cptc2026-tools/setup/bigmem-win.bat
@@ -0,0 +1,2 @@
+ren llamafile-0.9.3 llamafile-0.9.3.exe
+llamafile-0.9.3.exe -m Phi-3-mini-4k-instruct.Q4_K_S.llamafile --host 0.0.0.0 --port 28063
