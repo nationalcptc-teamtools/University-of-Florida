9d8d87f1e9f51d41c26b0f87f59edc43c80242a7
Nathan Padriga <73860587+0ntu@users.noreply.github.com>
2025-10-09 18:50:47 -0400

    Autostart asciinema

--- cptc2026-tools/setup/bigmem-lin.sh
+++
@@ -1,2 +0,0 @@
-chmod 744 llamafile-0.9.3
-./llamafile-0.9.3 -m Phi-3-mini-4k-instruct.Q4_K_S.llamafile --host 0.0.0.0 --port 28063
--- cptc2026-tools/setup/bigmem-win.bat
+++
@@ -1,2 +0,0 @@
-ren llamafile-0.9.3 llamafile-0.9.3.exe
-llamafile-0.9.3.exe -m Phi-3-mini-4k-instruct.Q4_K_S.llamafile --host 0.0.0.0 --port 28063
