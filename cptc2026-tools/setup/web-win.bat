e1d97373cecb8d99bbd6ef5d24058b0937b7c6c4
yhuang3-uf <115114652+yhuang3-uf@users.noreply.github.com>
2025-12-24 14:41:08 -0500

    Add web-related files

---
+++ cptc2026-tools/setup/web-win.bat
@@ -0,0 +1,4 @@
+mkdir %USERPROFLE%\Desktop\webrecorder
+cd /d %USERPROFILE%\Desktop\webrecorder
+curl.exe https://github.com/webrecorder/archiveweb.page/releases/download/v0.16.0/ArchiveWeb.page-0.16.0.exe -O
+curl.exe https://github.com/webrecorder/replayweb.page/releases/download/v2.4.0/ReplayWeb.page-2.4.0.exe -O
