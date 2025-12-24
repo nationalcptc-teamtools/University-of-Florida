7f917f7807750b1a14095b5303358b181134635f
yhuang3-uf <115114652+yhuang3-uf@users.noreply.github.com>
2025-12-24 14:49:31 -0500

    Fix bug in script

--- cptc2026-tools/setup/web-lin.sh
+++ cptc2026-tools/setup/web-lin.sh
@@ -1,6 +1,6 @@
 mkdir ~/webrecorder
 cd ~/webrecorder
-curl https://github.com/webrecorder/archiveweb.page/releases/download/v0.16.0/ArchiveWeb.page-0.16.0.AppImage -O
-curl https://github.com/webrecorder/replayweb.page/releases/download/v2.4.0/ReplayWeb.page-2.4.0.AppImage -O
+curl -L https://github.com/webrecorder/archiveweb.page/releases/download/v0.16.0/ArchiveWeb.page-0.16.0.AppImage -O
+curl -L https://github.com/webrecorder/replayweb.page/releases/download/v2.4.0/ReplayWeb.page-2.4.0.AppImage -O
 chmod +x ArchiveWeb.page-0.16.0.AppImage
 chmod +x ReplayWeb.page-2.4.0.AppImage 
